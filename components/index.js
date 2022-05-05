42c694cf620967f9fdf03c2fa042a3c0450f9bea
Mehtabkazmi <mehtabkazmi5@gmail.com>
2022-05-06 00:53:49 +0500

    updated data

---
+++ components/index.js
@@ -0,0 +1,3 @@
+export { default as HeroBanner } from './HeroBanner';
+export { default as Product } from './Product';
+export { default as FooterBanner } from './FooterBanner';
---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FooterBanner, HeroBanner, Product } from '../components';
+import { client } from '../lib/client'
+
+const Home = ({ products, bannerData }) => {
+  console.log(products);
+  return (
+    <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+      <div className='products-heading'>
+        <h2>Our best Products</h2>
+        <p>Mobile There are many variations passages</p>
+      </div>
+      <div className='products-container'>
+        {products?.map((item) => <Product key={item._id} product={item}/>)}
+      </div>
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type=="product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type=="banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+  return {
+    props:{products,bannerData}
+  }
+}
+
+export default Home
