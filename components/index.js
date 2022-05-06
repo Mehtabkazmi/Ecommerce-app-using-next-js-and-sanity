d9a01fa682eb94cff4ee99bc27fdbd78e4a9d15f
Mehtabkazmi <mehtabkazmi5@gmail.com>
2022-05-06 19:16:56 +0500

    updated data

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Footer } from './Footer';
 export { default as Product } from './Product';
 export { default as HeroBanner } from './HeroBanner';
 export { default as FooterBanner } from './FooterBanner';
+export { default as Navbar } from './Navbar';
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 import React from 'react'
+import Layout from '../components/Layout'
+import { Toaster } from 'react-hot-toast';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Toaster/>
+      <Component {...pageProps} />
+    </Layout>
+  )
+  
 }
 
 export default MyApp
